2b36742bc6d7fd577a0a0c6b30d7ded06887a087
Riccardo Giorato <giorat.96@gmail.com>
2018-10-23 09:08:07 +0200

    hand collision e blockout

---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour {
+
+	private Collider primoCollidato;
+
+	void OnTriggerEnter (Collider oggettoGrabbabile) {
+		if(!primoCollidato){
+			primoCollidato = oggettoGrabbabile;
+			primoCollidato.GetComponent<Renderer>().material.color = Color.blue;
+		}
+	}
+
+	void OnTriggerExit (Collider oggettoGrabbabile) {
+		if(oggettoGrabbabile == primoCollidato){
+			primoCollidato.GetComponent<Renderer>().material.color = Color.white;
+			primoCollidato = null;
+			}
+	}
+	void Update () {
+		if(primoCollidato)
+			Debug.Log(primoCollidato.name);
+	}
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -3 *40 * Time.deltaTime, 0 );
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 3*40 * Time.deltaTime, 0);
+        }
+        
+
+    }
+
+}
