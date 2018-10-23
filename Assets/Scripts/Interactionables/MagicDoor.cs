6d93a29878008322de8e722eb562584b31ed2305
Gabriel <ciulei.gabriel@gmail.com>
2018-10-23 09:23:00 +0200

    Added magic door

---
+++ Assets/Scripts/Interactionables/MagicDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicDoor : Interactionable, IUsable {
+
+    public bool CanBeUsed()
+    {
+        return true;
+    }
+
+    public string[] GetCollisionTags()
+    {
+        return new string[] { "UnlockKey" };
+    }
+
+    public void OnUse()
+    {
+        // TODO: open door
+        Debug.Log("Open door");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
