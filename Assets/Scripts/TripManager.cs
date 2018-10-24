ae5c83eb3f699ccf97e2b5f07c535940433ecf84
Unknown <noemail>
2018-10-24 16:58:01 +0200

    Potion and trip basic functionality

--- Assets/Scripts/TripManager.cs
+++ Assets/Scripts/TripManager.cs
@@ -8,29 +8,55 @@ public class TripManager : MonoBehaviour {
 
     private PlayerState   keyRequiredSize,
                           playerSize;
-    private bool isKeyGrabbed = false;
+    private bool isKeyGrabbed;
+
+    public GameObject[] keys, biscuits, potions, doors;
+    public GameObject keyEffect, biscuitEffect, potionEffect, doorEffect;
 
     private void MakeKeyShine() {
         Debug.Log("Key shining");
+        CreateEffectForObjects(keys, keyEffect);
     }
 
     private void MakeBiscuitShine() {
         Debug.Log("Biscuit shining");
+        CreateEffectForObjects(biscuits, biscuitEffect);
     }
 
     private void MakePotionShine()
     {
         Debug.Log("Potion shining");
+        CreateEffectForObjects(potions, potionEffect);
     }
 
     private void MakeDoorShine()
     {
         Debug.Log("Door shining");
+        CreateEffectForObjects(doors, doorEffect);
+    }
+
+    private void CreateEffectForObjects(GameObject[] objects, GameObject fromModel)
+    {
+        foreach (GameObject o in objects)
+        {
+            // add effect to object
+            CreateEffect(fromModel, o.transform);
+        }
+    }
+
+    private GameObject CreateEffect(GameObject fromModel, Transform parentTransform)
+    {
+        GameObject newEffect = Instantiate(fromModel, parentTransform);
+        newEffect.transform.localPosition = Vector3.zero;
+        return newEffect;
     }
 
     private void StopShining()
     {
         Debug.Log("Stop shining");
+        GameObject[] effects = GameObject.FindGameObjectsWithTag("trip_effect");
+        // destroy all effects
+        foreach (GameObject e in effects) Destroy(e);
     }
 
     #endregion
@@ -110,6 +136,7 @@ public class TripManager : MonoBehaviour {
         // setup guide
         keyRequiredSize = PlayerState.big;
         playerSize = PlayerState.medium;
+        isKeyGrabbed = false;
 
         TryMoveOn();
 	}
