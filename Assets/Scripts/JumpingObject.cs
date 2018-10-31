d65a09fb630d784585f816f58573ae6591917a42
Alessio Liuzzi <alessioliuzzi98@gmail.com>
2018-10-31 14:49:22 +0100

    Aggiornato script animali

--- Assets/Scripts/JumpingObject.cs
+++ Assets/Scripts/JumpingObject.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class JumpingObject : MonoBehaviour {
+public class JumpingObject : MonoBehaviour
+{
 
     public float minJumpHeight = 0.1f;
     public float maxJumpHeight = 0.5f;
     public int jumpNumber = 100;
     Sequence mySequence;
     Vector3 startPos;
-
+    public Transform destinationPos;
+    public float moveTime = 1f;
     private void Start()
     {
         startPos = transform.position;
@@ -18,20 +20,21 @@ public class JumpingObject : MonoBehaviour {
     private void OnEnable()
     {
         mySequence = DOTween.Sequence();
+        transform.DOMoveX(destinationPos.position.x, moveTime);
+        transform.DOMoveZ(destinationPos.position.z, moveTime).OnComplete(disableObject);
         for (int i = 0; i < jumpNumber; i++)
         {
-            
-                mySequence.Append(transform.DOMoveY(transform.position.y + Random.Range(minJumpHeight,maxJumpHeight), 0.5f))
-                .Append(transform.DOMoveY(transform.position.y, 0.5f));
-            
+
+            mySequence
+            .Append(transform.DOMoveY(transform.position.y + Random.Range(minJumpHeight, maxJumpHeight), 0.5f))
+            .Append(transform.DOMoveY(transform.position.y, 0.5f));
         }
-        
+
     }
 
-    private void OnDisable()
+    void disableObject()
     {
-        mySequence.Kill();
-        transform.position = startPos;
+        gameObject.SetActive(false);
     }
 
 }
