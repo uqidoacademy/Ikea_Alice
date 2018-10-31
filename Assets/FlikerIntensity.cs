dd1e362fe3ff28ea3a92527303542a81cd4a9d04
Simone <simone.magagna@studenti.unipd.it>
2018-10-31 16:43:00 +0100

    Light Fiker

---
+++ Assets/FlikerIntensity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlikerIntensity : MonoBehaviour {
+    public GameObject SmallLights;
+	public GameObject BigLights;
+    public float timer = 0.1f;
+	public float maxIntensity = 1.6f;
+	public float minIntensity = 1.5f;
+
+	private float currentIntensity;
+ 
+    void Start()
+    {
+        StartCoroutine(FlickeringLight());
+    }
+ 
+    IEnumerator FlickeringLight()
+    {
+        currentIntensity = Random.RandomRange(minIntensity, maxIntensity);
+		SmallLights.GetComponent<Light>().intensity = currentIntensity;
+		BigLights.GetComponent<Light>().intensity = currentIntensity;
+       
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(FlickeringLight());
+    }
+}
+
