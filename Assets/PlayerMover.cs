2dfef0c39b04f145911d77daca50b29ecdf0ca9f
Gabriel <ciulei.gabriel@gmail.com>
2018-11-06 14:14:30 +0100

    Player move

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+    public GameObject teleportLocationRoom;
+    public GameObject teleportLocationSmall;
+
+    public void goToSmallSpawn()
+    {
+        transform.position = teleportLocationSmall.transform.position;
+    }
+
+    public void goToRoomSpawn()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FadeScreen>().FadeEffectNow();
+        transform.DOMove(teleportLocationRoom.transform.position, 4);
+        transform.DORotate(teleportLocationRoom.transform.rotation.eulerAngles, 4, RotateMode.Fast);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
